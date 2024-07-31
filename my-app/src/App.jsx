f05ef89a88161fc5baecc0ae0bbee86b440030a7
gabrielaspooky <gabrielanava01@gmail.com>
2024-07-31 18:29:14 +0200

    added new info

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,6 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import LoadingScreen from './Components/LoadingScreen';
-import StarWarsCards from './Components/StarWarsCards';
+import StarWarsCards from './Components/StarWarsCards'; 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Suscripcion from './Pages/Suscripcion';
+import ParaEmpresa from './Pages/ParaEmpresa';
+import Tienda from './Pages/Tienda';
+import SobreNosotros from './Pages/SobreNosotros';
+import Contacto from './Pages/Contacto';
+import Nav from './Components/Nav';
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -15,15 +22,29 @@ function App() {
 
   return (
     <div className="App">
-      <LoadingScreen isVisible={isLoading} />
-      {!isLoading && (
-        <div className="flex items-center justify-center">
-         <StarWarsCards />
+      {isLoading ? (
+        <LoadingScreen isVisible={isLoading} />
+      ) : (
+        <div>
+          <div className="flex items-center justify-center">
+            <StarWarsCards />
+            <BrowserRouter>
+            <Nav/>
+            <Routes>
+                <Route path="/" element={<Tienda />} />
+                <Route path="/suscripcion" element={<Suscripcion />} />
+                <Route path="/paraempresa" element={<ParaEmpresa />} />
+                <Route path="/sobrenosotros" element={<SobreNosotros />} />
+                <Route path="/contacto" element={<Contacto />} />
+            </Routes>
+        </BrowserRouter>
+        <footer>Esto es un footer</footer>
+          </div>
         </div>
       )}
     </div>
   );
 }
 
-export default App;
 
+export default App;
